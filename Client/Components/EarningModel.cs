0b9ba9790aa3bcabfd43f59ecbb728feeed47a2f
IMI-TRISTAN <s.shillitoe@sheffield.ac.uk>
2023-11-05 02:12:35 +0000

    Add project files.

---
+++ Client/Components/EarningModel.cs
@@ -0,0 +1,13 @@
+﻿namespace FinanceMentor.Client.Components
+{
+    public class EarningModel
+    {
+        public DateTime Date { get; set; }
+        public string Subject { get; set; } = string.Empty;
+        public EarningCategory Category { get; set; }
+        public Decimal Amount { get; set; }
+
+
+
+    }
+}
